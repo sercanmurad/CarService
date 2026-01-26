01b3540471fc585849bb5252744732f505e5f4d7
Sercan Murad <sercanmuro.bg@gmail.com>
2026-01-26 12:50:38 +0200

    FırstVersıon

--- CoffeShop.DL/LocalDb/StaticDb.cs
+++ CoffeShop.DL/LocalDb/StaticDb.cs
@@ -1,14 +1,14 @@
-﻿using CarService.Models.Dto;
+using CoffeShop.Models.Dto;
 
-namespace CarService.DL.LocalDb
+namespace CoffeShop.DL.LocalDb
 {
     internal static class StaticDb
     {
-        public static List<Car> Cars = new List<Car>
+        public static List<Coffee> Coffees = new List<Coffee>
         {
-            new Car { Id = Guid.NewGuid(), Model = "Toyota Camry", Year = 2020 },
-            new Car { Id = Guid.NewGuid(), Model = "Honda Accord", Year = 2019 },
-            new Car { Id = Guid.NewGuid(), Model = "Ford Mustang", Year = 2021 }
+            new Coffee { Id = Guid.NewGuid(), Name = "Espresso", RoastYear = 2023, BasePrice = 3.50m },
+            new Coffee { Id = Guid.NewGuid(), Name = "Cappuccino", RoastYear = 2023, BasePrice = 4.20m },
+            new Coffee { Id = Guid.NewGuid(), Name = "Latte", RoastYear = 2023, BasePrice = 4.50m }
         };
 
         public static List<Customer> Customers =
